e74fcebccb4311c0c0b1eba1bbed721294003441
Nguyen Trung Chien <chienkt3112@gmail.com>
2024-05-15 05:51:55 +0000

    gitpod

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -24,9 +25,42 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.get('/api', (req, res) => {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString()
+  })
+})
+
+app.get('/api/:timestamp', (req, res) => {
+  const timestamp = req.params.timestamp
+
+  if(!isNaN(Number(timestamp)) && timestamp.length === 13){
+    return res.json({
+      unix: timestamp,
+      utc: new Date(Number(timestamp)).toUTCString()
+    })
+  }
+
+  if(new Date(timestamp).toUTCString() !== 'Invalid Date'){
+    return res.json({
+      unix: new Date(timestamp).getTime(),
+      utc: new Date(timestamp).toUTCString()
+    })
+  }else{
+    return res.json({
+      error: "Invalid Date"
+    })
+  }
+})
 
 
-// Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
+
+
+
+// Listen on port set in environment constiable or default to 3000
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
